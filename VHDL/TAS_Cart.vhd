6d06c58037b1d22e44c71ca5c3a7dee085201383
pjgat09@gmail.com <pjgat09@gmail.com@836ebc1e-bbf1-423a-eefa-8967d20eb370>
2013-10-31 04:18:51 +0000

    The NMI now goes to 0x8000 half the time, where I now inject 0x40 (RTI), effectively causing the console to run at half speed

--- VHDL/TAS_Cart.vhd
+++ VHDL/TAS_Cart.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity TAS_Cart is
     Port ( --CLK  : in STD_LOGIC;
            ADDR : in STD_LOGIC_VECTOR(14 downto 0);
-           DATA : out STD_LOGIC_VECTOR(7 downto 0);
+           DATA : inout STD_LOGIC_VECTOR(7 downto 0);
            RW   : in STD_LOGIC;
 			  M2   : in STD_LOGIC;
 			  CONSOLE_CE : in STD_LOGIC;
@@ -16,7 +16,8 @@ end TAS_Cart;
 architecture Behavioral of TAS_Cart is
 --	signal counter : integer range 0 to 959 := 0;
 --	signal led_output : std_logic := '0';
-   signal cart_disabled : std_logic := '0';
+--   signal cart_disabled : std_logic := '0';
+	signal injecting : std_logic := '0';
 begin
 
 --LED1 <= led_output;
@@ -24,19 +25,21 @@ begin
 flash_led: process(M2)
 	begin
 		if (rising_edge(M2)) then
-			if ('1' & ADDR = x"FFFA" or '1' & ADDR = x"FFFB") then
-				cart_disabled <= '1';
-			else
-				cart_disabled <= '0';
+			if ('1' & ADDR = x"FFFA") then
+				-- On the first NMI address read, toggle our state
+				injecting <= not injecting;
 			end if;
 		end if;
 	end process;
 
-CART_CE <= CONSOLE_CE when cart_disabled = '0' else
-			  '1';
+CART_CE <= '1' when (injecting = '1' and ('1' & ADDR = x"FFFA" or '1' & ADDR = x"FFFB" or '1' & ADDR = x"8000")) else
+           CONSOLE_CE;
 
-DATA <= "10000010" when (cart_disabled = '1' and not CONSOLE_CE & ADDR = x"FFFA" and RW = '1') else
-        "10000000" when (cart_disabled = '1' and not CONSOLE_CE & ADDR = x"FFFB" and RW = '1') else
+DATA <= "01000000" when (injecting = '1' and '1' & ADDR = x"8000" and RW = '1') else
+        "00000000" when (injecting = '1' and not CONSOLE_CE & ADDR = x"FFFA" and RW = '1') else
+        "10000000" when (injecting = '1' and not CONSOLE_CE & ADDR = x"FFFB" and RW = '1') else
+        "10000010" when (injecting = '0' and not CONSOLE_CE & ADDR = x"FFFA" and RW = '1') else
+        "10000000" when (injecting = '0' and not CONSOLE_CE & ADDR = x"FFFB" and RW = '1') else
         "ZZZZZZZZ";
 
 end Behavioral;
