c0908f58f8b08dcaae7c27235f40f027f3984983
pjgat09@gmail.com <pjgat09@gmail.com@836ebc1e-bbf1-423a-eefa-8967d20eb370>
2013-11-04 03:48:39 +0000

    Moved the UART code to it's own file and re-wrote it to indicate when a new byte is available. Changed the main code to use the new module, and implemented the new byte detection to work as a frame advance. Also changed the injection so no data is output when not injected (fixes SMB3 and really most other games that don't use 0x8082 for their NMI)

--- trunk/VHDL/TAS_Cart.vhd
+++ trunk/VHDL/TAS_Cart.vhd
@@ -15,62 +15,52 @@ entity TAS_Cart is
 end TAS_Cart;
 
 architecture Behavioral of TAS_Cart is
+	component UART is
+    Port ( rx_data_out : out STD_LOGIC_VECTOR (7 downto 0);
+           rx_data_was_recieved : in STD_LOGIC;
+           rx_byte_waiting : out STD_LOGIC;
+           clk : in  STD_LOGIC;
+			  rx_in : in STD_LOGIC;
+			  LED : out STD_LOGIC);
+	end component;
+
 --	signal counter : integer range 0 to 959 := 0;
-	signal baud_counter : integer range 0 to 1627 := 0;
-	signal rx_bit_buffer : std_logic_vector (39 downto 0) := (others => '1');
-	signal led_output : std_logic := '0';
 --   signal cart_disabled : std_logic := '0';
 	signal injecting : std_logic := '0';
-	signal baud : std_logic := '0';
-	signal recent_rx_val : std_logic_vector(7 downto 0);
+	
+	signal data_from_uart : STD_LOGIC_VECTOR (7 downto 0);
+	signal uart_data_recieved : STD_LOGIC := '0';
+	signal uart_byte_waiting : STD_LOGIC := '0';
 begin
 
-baud_generator: process (CLK)
-	begin
-		if rising_edge(CLK) then
-			if (baud_counter = 407) then
-				baud <= not baud;
-				baud_counter <= 0;
-			else
-				baud_counter <= baud_counter + 1;
-			end if;
-		end if;
-	end process;
+uart1: UART port map (rx_data_out => data_from_uart,
+							 rx_data_was_recieved => uart_data_recieved,
+							 rx_byte_waiting => uart_byte_waiting,
+							 clk => CLK,
+							 rx_in => UART_RX,
+							 LED => LED1);
 
-RX_bit: process (baud)
-	begin
-		if rising_edge(baud) then
-			if (rx_bit_buffer(38 downto 37) = "11" and
-			    rx_bit_buffer(34) = rx_bit_buffer(33) and
-				 rx_bit_buffer(30) = rx_bit_buffer(29) and
-				 rx_bit_buffer(26) = rx_bit_buffer(25) and
-				 rx_bit_buffer(22) = rx_bit_buffer(21) and
-				 rx_bit_buffer(18) = rx_bit_buffer(17) and
-				 rx_bit_buffer(14) = rx_bit_buffer(13) and
-				 rx_bit_buffer(10) = rx_bit_buffer(9) and
-				 rx_bit_buffer(6) = rx_bit_buffer(5) and
-				 rx_bit_buffer(2 downto 1) = "00") then
-				 recent_rx_val <= rx_bit_buffer(34) & rx_bit_buffer(30) & rx_bit_buffer(26) & rx_bit_buffer(22) & rx_bit_buffer(18) & rx_bit_buffer(14) & rx_bit_buffer(10) & rx_bit_buffer(6);
-				 led_output <= not led_output;
-				 rx_bit_buffer <= (others => '1');
-			else
-				rx_bit_buffer <= UART_RX & rx_bit_buffer(39 downto 1);
-			end if;
-		end if;
-	end process;
-	
-LED1 <= led_output;
+
+
 
 flash_led: process(M2)
 	begin
 		if (rising_edge(M2)) then
 			if ('1' & ADDR = x"FFFA") then
 				-- On the first NMI address read, toggle our state
-				if (recent_rx_val = x"31") then
-					injecting <= not injecting;
+				if (uart_byte_waiting = '1') then
+					--if (data_from_uart = x"31") then
+						injecting <= '0';
+					--else
+					--	injecting <= '1';
+					--end if;
+					uart_data_recieved <= '1';
 				else
-					injecting <= '0';
+					injecting <= '1';
+					uart_data_recieved <= '0';
 				end if;
+			else
+				uart_data_recieved <= '0';
 			end if;
 		end if;
 	end process;
@@ -81,8 +71,8 @@ CART_CE <= '1' when (injecting = '1' and ('1' & ADDR = x"FFFA" or '1' & ADDR = x
 DATA <= "01000000" when (injecting = '1' and '1' & ADDR = x"8000" and RW = '1') else
         "00000000" when (injecting = '1' and not CONSOLE_CE & ADDR = x"FFFA" and RW = '1') else
         "10000000" when (injecting = '1' and not CONSOLE_CE & ADDR = x"FFFB" and RW = '1') else
-        "10000010" when (injecting = '0' and not CONSOLE_CE & ADDR = x"FFFA" and RW = '1') else
-        "10000000" when (injecting = '0' and not CONSOLE_CE & ADDR = x"FFFB" and RW = '1') else
+        --"10000010" when (injecting = '0' and not CONSOLE_CE & ADDR = x"FFFA" and RW = '1') else
+        --"10000000" when (injecting = '0' and not CONSOLE_CE & ADDR = x"FFFB" and RW = '1') else
         "ZZZZZZZZ";
 
 end Behavioral;
